0d334edbec28f618306bf393f35eade9af4c9efa
Gemfield <gemfield@civilnet.cn>
2020-09-14 18:46:10 +0800

    put img2tensor API to utils dir

--- loader/src/deepvac_loader.cpp
+++ loader/src/deepvac_loader.cpp
@@ -11,35 +11,9 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "deepvac_loader.h"
 #include "gemfield.h"
+#include "syszux_img2tensor.h"
 
 namespace deepvac{
-
-std::optional<cv::Mat> img2CvMat(std::string& img_path, bool is_rgb){
-    cv::Mat frame;
-    try{
-        frame = cv::imread(img_path);
-    }catch(std::exception& e){
-        GEMFIELD_E2("Error to read img: ", img_path.c_str());
-        return std::nullopt;
-    }
-    if (frame.rows == 0 or frame.cols == 0){
-        GEMFIELD_E2("illegal img: ", img_path.c_str());
-        return std::nullopt;
-    }
-    if(is_rgb){
-        cv::cvtColor(frame, frame, CV_BGR2RGB);
-    }
-    frame.convertTo(frame, CV_32FC3, 1.0f / 255.0f);
-    return frame;
-}
-
-std::optional<at::Tensor> img2tensor(std::string& img_path, bool is_rgb){
-    cv::Mat frame = img2CvMat(img_path, is_rgb).value();
-    auto input_tensor = torch::from_blob(frame.data, {1, frame.rows, frame.cols, 3});
-    input_tensor = input_tensor.permute({0, 3, 1, 2});
-    input_tensor = input_tensor.sub_(0.5).div_(0.5);
-    return input_tensor;
-}
 
 const std::string RecursiveFileIter::operator*() const {
     std::string p = (*walker_).path(); 
@@ -48,7 +22,7 @@ const std::string RecursiveFileIter::operator*() const {
 
 const std::optional<cv::Mat> CvMatIter::operator*() const {
     std::string p = (*walker_).path();
-    return img2CvMat(p);
+    return gemfield_org::img2CvMat(p);
 }
 
 const std::tuple<std::string, std::optional<at::Tensor>> ImgFileInputTensorPairIter::operator*() const {
@@ -65,12 +39,12 @@ const std::tuple<std::string, std::optional<at::Tensor>> ImgFileInputTensorPairI
         return {p, std::nullopt};
     }
 
-    return {p, img2tensor(p)};
+    return {p, gemfield_org::img2tensor(p)};
 }
 
 //loader that based on std container.
 std::optional<cv::Mat> DeepvacVectorIter::operator*() const {
-    return img2CvMat(*walker_);
+    return gemfield_org::img2CvMat(*walker_);
 }
 
 } //namespace deepvac
