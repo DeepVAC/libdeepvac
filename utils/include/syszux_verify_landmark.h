0f6b77402ea1423397d6eb94fa0bb1b39660a249
wyh163 <317488556@qq.com>
2020-09-15 16:57:06 +0800

    add filter face (#2)
    * add filter_face
    
    * add filter face
    
    * add verify landmark

---
+++ utils/include/syszux_verify_landmark.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 Gemfield <gemfield@civilnet.cn>
+ * This file is part of libdeepvac, licensed under the GPLv3 (the "License")
+ * You may not use this file except in compliance with the License.
+ */
+
+#pragma once
+#include "opencv2/opencv.hpp"
+#include <vector>
+
+namespace gemfield_org{
+    bool isValidLandmark(std::vector<float> bbox, std::vector<float> landmark, int min_face_size=24);
+}
