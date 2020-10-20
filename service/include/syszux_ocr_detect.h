a1114b3d2dccfa721850ac202fd3ecb228f443ae
wyh163 <317488556@qq.com>
2020-10-20 14:56:22 +0800

    add merge box (#9)
    * add ocr detect
    
    * add ocr recog
    
    * delete recog ocr
    
    * merge adaptor & ocr_detect
    
    * cut opencv
    
    * readme
    
    * CV_BRG2RGB -> cv::COLOR_BGR2RGB
    
    * inherit SyszuxOcrDetect from Deepvac
    
    * inherit SyszuxOcrDetect from Deepvac
    
    * add detect model
    
    * bug fix
    
    * bug fix
    
    * add merge box
    
    * add merge box
    
    * change Camel-Case for function
    
    Co-authored-by: SIAKAM <wangyuhang@deepait.com>

--- service/include/syszux_ocr_detect.h
+++ service/include/syszux_ocr_detect.h
@@ -10,6 +10,7 @@
 #include <torch/script.h>
 #include "opencv2/opencv.hpp"
 #include <queue>
+#include <algorithm>
 
 namespace deepvac {
 class SyszuxOcrDetect : public Deepvac {
@@ -20,12 +21,14 @@ class SyszuxOcrDetect : public Deepvac {
         SyszuxOcrDetect(SyszuxOcrDetect&&) = default;
         SyszuxOcrDetect& operator=(SyszuxOcrDetect&&) = default;
         virtual ~SyszuxOcrDetect() = default;
-        virtual std::optional<std::vector<cv::Mat>> operator() (cv::Mat frame);
+        virtual std::optional<std::pair<std::vector<cv::Mat>, std::vector<std::vector<int>>>> operator() (cv::Mat frame);
         void set(int long_size, int gap);
     private:
-        void get_kernals(torch::Tensor input_data, std::vector<cv::Mat> &kernals);
-        void growing_text_line(std::vector<cv::Mat> &kernals, std::vector<std::vector<int>> &text_line, float min_area);
-        std::vector<std::vector<int>> adaptor_pse(torch::Tensor input_data, float min_area);
+        void getKernals(torch::Tensor input_data, std::vector<cv::Mat> &kernals);
+        void growingTextLine(std::vector<cv::Mat> &kernals, std::vector<std::vector<int>> &text_line, float min_area);
+        std::vector<std::vector<int>> adaptorPse(torch::Tensor input_data, float min_area);
+        bool isMerge(std::vector<float> rect1, std::vector<float> rect2);
+        std::vector<std::vector<float>> mergeBox(std::vector<std::vector<float>> rects);
     private:
         int long_size_;
         int crop_gap_;
