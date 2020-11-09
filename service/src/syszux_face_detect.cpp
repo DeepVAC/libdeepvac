58e8d0d424ba87cb5f86bee4bd3fa1ecf7965e59
wyh163 <31748856@qq.com>
2020-11-09 18:48:25 +0800

    fix face bug

--- service/src/syszux_face_detect.cpp
+++ service/src/syszux_face_detect.cpp
@@ -14,7 +14,7 @@
 
 namespace deepvac{
 
-SyszuxFaceDetect::SyszuxFaceDetect(std::string device):Deepvac(facedet_deepvac, device),
+SyszuxFaceDetect::SyszuxFaceDetect(std::string device):Deepvac("/gemfield/hostpv/gemfield/facedet.deepvac", device),
     prior_box_({{16,32},{64,128},{256,512}}, {8,16,32}){}
 
 std::optional<std::vector<cv::Mat>> SyszuxFaceDetect::operator()(cv::Mat frame){
@@ -41,6 +41,7 @@ std::optional<std::vector<cv::Mat>> SyszuxFaceDetect::operator()(cv::Mat frame){
     input_tensor[0][0] = input_tensor[0][0].sub_(104);
     input_tensor[0][1] = input_tensor[0][1].sub_(117);
     input_tensor[0][2] = input_tensor[0][2].sub_(123);
+
     
     auto output = forwardTuple(input_tensor);
     //Nx4    //Nx2    //Nx10
@@ -119,6 +120,7 @@ std::optional<std::vector<cv::Mat>> SyszuxFaceDetect::operator()(cv::Mat frame){
     for(int i=0; i<landms_mat.rows; i++){
         auto landmark = landms_mat.row(i);
         cv::Mat dst_img = align_face_(frame_ori, landmark);
+	dst_img.convertTo(dst_img, CV_32FC1);
         detect_vec.push_back(dst_img);
     }
     return detect_vec;
--- utils/src/syszux_align_face.cpp
+++ utils/src/syszux_align_face.cpp
@@ -7,31 +7,34 @@
 #include <vector>
 #include "gemfield.h"
 #include "syszux_align_face.h"
+#include <torch/script.h>
+#include <torch/torch.h>
 
 namespace gemfield_org{
 AlignFace::AlignFace()
 {
-    float REFERENCE_FACIAL_POINTS_112x112[5][2] = {
+    double REFERENCE_FACIAL_POINTS_112x112[5][2] = {
 	{38.29459953, 51.69630051},
 	{73.53179932, 51.50139999},
 	{56.02519989, 71.73660278},
 	{41.54930115, 92.3655014},
 	{70.72990036, 92.20410156}
     };
-    ref_facial_5pts_ = cv::Mat(5, 2, CV_32FC1, &REFERENCE_FACIAL_POINTS_112x112).clone();
+    ref_facial_5pts_ = cv::Mat(5, 2, CV_64F, &REFERENCE_FACIAL_POINTS_112x112).clone();
 
-    float CROP_SIZE[2] = {112,112};
-    crop_size_ = cv::Mat(1,2,CV_32FC1,&CROP_SIZE).clone();
+    double CROP_SIZE[2] = {112,112};
+    crop_size_ = cv::Mat(1,2,CV_64F,&CROP_SIZE).clone();
 }
 
 cv::Mat AlignFace::operator() (cv::Mat& frame, cv::Mat& facial_5pts)
 {
     cv::Mat x, y;
     cv::Mat facial;
+    facial_5pts.convertTo(facial_5pts, CV_64F);
     //x1,y1,x2,y2... somebody familiar with cv::Mat, please refactor this code snippet.
     for(int i=0; i<facial_5pts.cols; i=i+2){
-        x.push_back(facial_5pts.at<float>(i));
-        y.push_back(facial_5pts.at<float>(i+1));
+        x.push_back(facial_5pts.at<double>(i));
+        y.push_back(facial_5pts.at<double>(i+1));
     }
     
     cv::hconcat(x, y, facial);
@@ -45,11 +48,11 @@ cv::Mat AlignFace::warpAndCrop(cv::Mat& src_img, cv::Mat& facial_5pts)
         GEMFIELD_E(msg.c_str());
         throw std::runtime_error(msg);
 	}
-	
+
     cv::Mat tfm = getAffineTransform(facial_5pts);
 
     cv::Mat dst_img;
-    cv::warpAffine(src_img, dst_img, tfm, cv::Size(int(crop_size_.at<float>(0)), int(crop_size_.at<float>(1))));
+    cv::warpAffine(src_img, dst_img, tfm, cv::Size(int(crop_size_.at<double>(0)), int(crop_size_.at<double>(1))));
 
     return dst_img;
 }
@@ -57,21 +60,21 @@ cv::Mat AlignFace::warpAndCrop(cv::Mat& src_img, cv::Mat& facial_5pts)
 cv::Mat AlignFace::getAffineTransform(cv::Mat& facial_5pts)
 {
     cv::Mat trans1 = findNonereflectiveSimilarity(facial_5pts, ref_facial_5pts_);
-	
+    
     cv::Mat xyR = ref_facial_5pts_.clone();
     xyR.col(0) = -1 * xyR.col(0);
 
     cv::Mat trans2r = findNonereflectiveSimilarity(facial_5pts, xyR);
     
-    cv::Mat TreflectY = cv::Mat::eye(3, 3, CV_32F);
-    TreflectY.at<float>(0, 0) = -1;
-	
+    cv::Mat TreflectY = cv::Mat::eye(3, 3, CV_64F);
+    TreflectY.at<double>(0, 0) = -1.;
+
     cv::Mat trans2 = trans2r * TreflectY;
 
     cv::Mat xy1 = tformfwd(trans1, facial_5pts);
-    float norm1 = cv::norm(xy1, ref_facial_5pts_);
+    double norm1 = cv::norm(xy1, ref_facial_5pts_);
     cv::Mat xy2 = tformfwd(trans2, facial_5pts);
-    float norm2 = cv::norm(xy2, ref_facial_5pts_);
+    double norm2 = cv::norm(xy2, ref_facial_5pts_);
 
     cv::Mat trans;
     if(norm1 <= norm2){
@@ -90,8 +93,8 @@ cv::Mat AlignFace::findNonereflectiveSimilarity(cv::Mat& facial_5pts, cv::Mat& r
     cv::Mat y = ref_facial_5pts.col(1);
 
     cv::Mat temp1, temp2, X;
-    std::vector<cv::Mat> matrices1 = {x, y, cv::Mat::ones(M, 1, CV_32F), cv::Mat::zeros(M, 1, CV_32F)};
-    std::vector<cv::Mat> matrices2 = {y, -x, cv::Mat::zeros(M, 1, CV_32F), cv::Mat::ones(M, 1, CV_32F)};
+    std::vector<cv::Mat> matrices1 = {x, y, cv::Mat::ones(M, 1, CV_64F), cv::Mat::zeros(M, 1, CV_64F)};
+    std::vector<cv::Mat> matrices2 = {y, -x, cv::Mat::zeros(M, 1, CV_64F), cv::Mat::ones(M, 1, CV_64F)};
     
     cv::hconcat(matrices1, temp1);
     cv::hconcat(matrices2, temp2);
@@ -102,23 +105,24 @@ cv::Mat AlignFace::findNonereflectiveSimilarity(cv::Mat& facial_5pts, cv::Mat& r
     cv::Mat v = facial_5pts.col(1);
     cv::vconcat(u, v, U);
 
-    cv::Mat s_r, u_r, v_r;
-    cv::SVD::compute(X, s_r, u_r, v_r);
-    if(s_r.rows < 2 * K){
-        throw std::runtime_error("cp2tform:twoUniquePointsReq");
-    }
-    cv::Mat r;
-    cv::solve(X, U, r, cv::DECOMP_SVD);
-    float sc = r.at<float>(0, 0);
-    float ss = r.at<float>(1, 0);
-    float tx = r.at<float>(2, 0);
-    float ty = r.at<float>(3, 0);
+    
+    U.convertTo(U, CV_32F);
+    X.convertTo(X, CV_32F);
+    auto U_tensor = torch::from_blob(U.data,{U.rows, U.cols});
+    auto X_tensor = torch::from_blob(X.data,{X.rows, X.cols});
+    auto res = torch::lstsq(U_tensor, X_tensor);
+    torch::Tensor r = std::get<0>(res);
 
-    float Tinv_vec[3][3] = {{sc, -ss, 0.},{ss, sc, 0.},{tx, ty, 1.}};
-    cv::Mat trans_inv = cv::Mat(3, 3, CV_32F, Tinv_vec);
+    double sc = (double)(r[0].item<float>());
+    double ss = (double)(r[1].item<float>());
+    double tx = (double)(r[2].item<float>());
+    double ty = (double)(r[3].item<float>());
+    
+    double Tinv_vec[3][3] = {{sc, -ss, 0.},{ss, sc, 0.},{tx, ty, 1.}};
+    cv::Mat trans_inv = cv::Mat(3, 3, CV_64F, Tinv_vec);
     cv::Mat trans = trans_inv.inv();
 
-    std::vector<float> tmp_vec = {0, 0, 1};
+    std::vector<double> tmp_vec = {0, 0, 1};
     cv::Mat tmp(tmp_vec);
     tmp = tmp.t();
     trans.col(2) = tmp;
@@ -127,7 +131,7 @@ cv::Mat AlignFace::findNonereflectiveSimilarity(cv::Mat& facial_5pts, cv::Mat& r
 
 cv::Mat AlignFace::tformfwd(cv::Mat& trans, cv::Mat facial_5pts)
 {
-    cv::hconcat(facial_5pts, cv::Mat::ones(facial_5pts.rows, 1, CV_32F), facial_5pts);
+    cv::hconcat(facial_5pts, cv::Mat::ones(facial_5pts.rows, 1, CV_64F), facial_5pts);
     cv::Mat res = facial_5pts * trans;
     return res.colRange(0, 2);
 }
