7bbaa08dc13bdc0addd148029a2f7cd2a98a007c
Gemfield <gemfield@civilnet.cn>
2020-12-02 12:28:33 +0000

    WIP: update cmake to apply libtorch static

--- cmake/deepvac-config.cmake
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2020 Gemfield <gemfield@civilnet.cn>
-# This file is part of libdeepvac, licensed under the GPLv3 (the "License")
-# You may not use this file except in compliance with the License.
-
-include("${CMAKE_CURRENT_LIST_DIR}/deepvac.cmake")
