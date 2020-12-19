c9e7b53dffd432ccba5519069cc544c0f14f1767
MHGL <1545989994@qq.com>
2020-12-19 12:19:32 +0000

    add cls resnet

---
+++ modules/include/syszux_cls_resnet.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 Gemfield <gemfield@civilnet.cn>
+ * This file is part of libdeepvac, licensed under the GPLv3 (the "License")
+ * You may not use this file except in compliance with the License.
+ */
+
+#pragma once
+
+#include "deepvac.h"
+#include "syszux_img2tensor.h"
+
+namespace deepvac{
+class SyszuxClsResnet : public Deepvac{
+    public:
+        SyszuxClsResnet(std::string path, std::string device = "cpu");
+        SyszuxClsResnet(const SyszuxClsResnet&) = delete;
+        SyszuxClsResnet& operator=(const SyszuxClsResnet&) = delete;
+        SyszuxClsResnet(SyszuxClsResnet&&) = default;
+        SyszuxClsResnet& operator=(SyszuxClsResnet&&) = default;
+        virtual ~SyszuxClsResnet() = default;
+        std::optional<std::pair<int, float>> process(cv::Mat frame);
+        void set(std::vector<int> input_size);
+    private:
+        std::vector<int> input_size_;
+};
+} //namespace deepvac
---
+++ modules/src/syszux_cls_resnet.cpp
@@ -0,0 +1,43 @@
+/*
+* Copyright (c) 2020 Gemfield <gemfield@civilnet.cn>
+* This file is part of libdeepvac, licensed under the GPLv3 (the "License")
+* You may not use this file except in compliance with the License.
+*/
+#include "syszux_cls_resnet.h"
+#include "gemfield.h"
+
+namespace deepvac{
+
+SyszuxClsResnet::SyszuxClsResnet(std::string path, std::string device):Deepvac(path, device) {}
+
+void SyszuxClsResnet::set(std::vector<int> input_size) {
+    input_size_ = input_size;
+}
+
+std::optional<std::pair<int, float>> SyszuxClsResnet::process(cv::Mat frame){
+    cv::Mat input_frame = frame.clone();
+    cv::cvtColor(input_frame, input_frame, cv::COLOR_BGR2RGB);
+    cv::resize(input_frame, input_frame, cv::Size(input_size_[0], input_size_[1]));
+
+    input_frame.convertTo(input_frame, CV_32FC3);
+    auto input_tensor_opt = gemfield_org::cvMat2Tensor(input_frame, gemfield_org::NORMALIZE0_1, gemfield_org::MEAN_STD_FROM_IMAGENET);
+    if(!input_tensor_opt){
+        return std::nullopt;
+    }
+    auto input_tensor = input_tensor_opt.value();
+    
+    input_tensor = input_tensor.to(device_);
+    input_tensor = input_tensor.toType(torch::kFloat32);
+    
+    auto output = forward(input_tensor);
+    
+    auto softmaxs = output.softmax(1);
+    std::tuple<torch::Tensor, torch::Tensor> max_res = torch::max(softmaxs, 1);
+    
+    auto max_probability = std::get<0>(max_res).item<float>();
+    auto index = std::get<1>(max_res).item<int>();
+
+    std::pair result(index, max_probability);
+    return result;
+}
+} //namespace deepvac
